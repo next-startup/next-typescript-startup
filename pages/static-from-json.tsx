4529d07aace53b13744c023dc9cab3e5d6cc18e2
Yokiijay <yokiijay@gmail.com>
2020-07-05 08:28:26 +0800

    Update static-from-json.tsx

--- pages/static-from-json.tsx
+++ pages/static-from-json.tsx
@@ -2,17 +2,16 @@ import Layout from "../components/Layout"
 import { GetStaticProps } from "next"
 import users from '../data/users.json'
 import { FC } from "react"
-import { log } from "console"
 
 const StaticFromJsonDemoPage: FC<StaticProps> = ({users}) => {
   return(
     <Layout style={{ justifyContent: 'flex-start' }}>
-      {users.map(user=><User {...user} />)}
+      {users.map(user=><User key={user.id} {...user} />)}
     </Layout>
   )
 }
 
-const User: FC<User> = ({name, email, id, website})=><div key={id} style={{ textAlign: 'center' }}>
+const User: FC<User> = ({name, email, website})=><div style={{ textAlign: 'center' }}>
   <h1>{name}</h1>
   <a href={`mailto:${email}`}>{email}</a>
   <p><a href={website}><strong>{website}</strong></a></p>
